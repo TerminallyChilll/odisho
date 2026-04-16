cc3c74f180a7490cb7b1fbdb85041221625aedb1
Tom M <73926467+TerminallyChilll@users.noreply.github.com>
2026-04-16 17:10:10 -0400

    Switch to HashRouter and relative base path for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
 import { AboutPage, ProductsPage } from './components/Pages';
 
 export default function App() {
   return (
-    <BrowserRouter basename="/odisho">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/products" element={<ProductsPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
